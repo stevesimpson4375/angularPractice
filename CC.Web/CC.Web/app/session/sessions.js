fa59bd49e5516d18bad33e3b98410470386be05b
steve simpson <stevesimpson4375@gmail.com>
2017-05-15 22:25:32 -0400

    Progress Update

---
+++ CC.Web/CC.Web/app/session/sessions.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('sessions', sessions);
+
+    sessions.$inject = ['common']; 
+
+    function sessions(common) {
+        /* jshint validthis:true */
+        var vm = this;
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        vm.sessions = [];
+        vm.title = 'sessions';
+
+        activate();
+
+        function activate() { }
+        //TODO: get our seesions
+        function activate() {
+            common.activateController([getSessions()], controllerId)
+                .then(function () { log('Activated Session View'); });
+        }
+
+        function getSessions() {
+
+        }
+    }
+})();
